e82a2f34aae9cd33c201e9387bbdde70e8de6627
Harish-Kushwah <harishkushwah54321@gmail.com>
2024-10-12 21:41:33 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 function toggleImage(){
     const image = document.getElementById("toggleImage");
     if(!image.classList.contains("activeMenu")){
-          image.src = "/asets/close.png";
+          image.src = "asets/close.png";
           image.classList.add("activeMenu");
     }
     else{
-        image.src = "/asets/open.png";
+        image.src = "asets/open.png";
         image.classList.remove("activeMenu");
     }
 }
