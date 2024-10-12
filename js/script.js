b964f5bb8e89c30682c147cd0030e11095a671f0
Harish-Kushwah <harishkushwah54321@gmail.com>
2024-10-12 21:31:26 +0530

    Home page added

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function toggleImage(){
+    const image = document.getElementById("toggleImage");
+    if(!image.classList.contains("activeMenu")){
+          image.src = "/asets/close.png";
+          image.classList.add("activeMenu");
+    }
+    else{
+        image.src = "/asets/open.png";
+        image.classList.remove("activeMenu");
+    }
+}
