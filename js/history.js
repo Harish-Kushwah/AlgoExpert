6a6f87d25dbb4f8fd1c1d160a1abcaacc3994675
Harish-Kushwah <harishkushwah54321@gmail.com>
2024-11-04 23:46:19 +0530

    visualization history feature UI added

---
+++ js/history.js
@@ -0,0 +1,49 @@
+function getHistoryHTML(history){
+    return ` <div class="history-field" id=${history.id}>
+          <div class="left-section">
+            <div class="status-icons">
+               <img src="./asets/${history.status_icon}" alt="" width="16px" height="16px"/>
+            </div>
+          </div>
+          <div class="middle-section">
+            <div class="number">
+              <p>${history.numbers}</p>
+            </div>
+            <div class="des">
+              <div class="algorithm">
+                <p>${history.algorithm}</p>
+              </div>
+              <div class="time">
+                <p>${history.date}</p>
+              </div>
+            </div>
+          </div>
+          <div class="right-section">
+            <div class="status-icons" onclick="removeHistory(${history.id})">
+              <img src="./asets/delete.png" alt="" width="30px" height="30px" class="delete-icon"/>
+           </div>
+          </div>
+        </div>`;
+}
+
+
+
+
+const historyContainer = document.getElementById('history-container');
+
+function addHistory(allHistory){
+    allHistory.forEach(history => {
+        historyContainer.innerHTML+=getHistoryHTML(history);
+    
+    });
+}
+
+addHistory(allHistory);
+
+function removeHistory(id){
+    historyContainer.innerHTML = '';
+    allHistory = allHistory.filter((history)=>{
+       return history.id !=id;
+    })
+    addHistory(allHistory);
+}
